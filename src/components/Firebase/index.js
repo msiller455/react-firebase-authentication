5d655dc9c85b0e102ce5686e068e3a4f9c7ffb36
Michael <msiller455@gmail.com>
2020-04-30 13:35:50 -0700

    adds firebase setup and signup page

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
+import FirebaseContext, { withFirebase } from './context'
+import Firebase from './firebase'
 
-const Firebase = () => {
-    return (
-        <div>
-            <h1>Firebase</h1>
-        </div>
-    );
-};
-
-export default Firebase;
+export default Firebase
+export { FirebaseContext, withFirebase }
